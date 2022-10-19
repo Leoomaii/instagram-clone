757abb27b4bf15f1f8f68049f9b32e389ad69d5b
Leonardo <leonardo-mai@outlook.com>
2022-10-19 14:59:26 +1100

    upload photo function complete

--- components/Posts.js
+++ components/Posts.js
@@ -1,46 +1,65 @@
 import React from 'react'
-import Post from './Post'
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import Post from './Post';
 
-const posts = [
-    {
-        id:'123',
-        username: 'leoomaii',
-        userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        caption: "new dp",
-    },
-    {
-        id:'123',
-        username: 'leoomaii',
-        userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        caption: "new dp",
-    },
-    {
-        id:'123',
-        username: 'leoomaii',
-        userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
-        caption: "new dp",
-    },
-]
+// const posts = [
+//     {
+//         id:'123',
+//         username: 'leoomaii',
+//         userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         caption: "new dp",
+//     },
+//     {
+//         id:'123',
+//         username: 'leoomaii',
+//         userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         caption: "new dp",
+//     },
+//     {
+//         id:'123',
+//         username: 'leoomaii',
+//         userImg:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         img:"https://cdn.sanity.io/images/s4gdqrgm/production/9ca217c3d8e21d8fca561e502d56941584707a12-828x808.jpg",
+//         caption: "new dp",
+//     },
+// ]
 
 
 
 function Posts() {
-  return (
-    <div>
-        {posts.map((post) => (
-            <Post key={post.id}
-            username={post.username}
-            userImg={post.userImg}
-            img={post.img}
-            caption={post.caption}
-            
-            />
-        ))}
-    </div>
-  )
-}
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const unsubcribe = onSnapshot(
+            query(collection(db, "posts"), orderBy("timestamp", "desc")),
+            (snapshot) => {
+                setPosts(snapshot.docs);
+            }
+        );
+        return () => {
+            unsubcribe();
+            };
+        }, [db]);
+
+
+        return (
+            <div>
+                {posts.map((post) => (
+                    <Post
+                    key={post.id}
+                    id={post.id}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
+                    />
+                ))}
+            </div>
+        );
+    }
 
 export default Posts
--- components/Stories.js
+++ components/Stories.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import Story from "./Story";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function Stories() {
   const [suggestions, setSuggestions] = useState([]);
-  const {data: session } =useSession();
+  const { data: session } = useSession();
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       userId: faker.datatype.uuid(),
@@ -17,24 +17,23 @@ function Stories() {
       registeredAt: faker.date.past(),
     }));
     setSuggestions(suggestions);
-    console.log(suggestions)
+    console.log(suggestions);
   }, []);
 
   return (
-    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black'>
-    {session && (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thumb-black">
+      {session && (
         <Story img={session.user.image} username={session.user.username} />
-    )}
+      )}
 
-    
-        {suggestions.map(profile => (
-            <Story 
-            key={profile.id} 
-            img={profile.avatar} 
-            username={profile.username} />
-        ))}
-        
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
-)
+  );
 }
 export default Stories;
